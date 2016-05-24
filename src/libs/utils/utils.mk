e8300d11de8c78bf328f822462d4f10f62452686
Tobias Neumann <t.neumann@fh-aachen.de>
2016-05-24 16:29:07 +0200

    utils: utils.mk add shm and clock feature check

--- src/libs/utils/utils.mk
+++ src/libs/utils/utils.mk
@@ -3,6 +3,7 @@
 #                            -------------------
 #   Created on Mon Mar 10 11:12:14 2008
 #   Copyright (C) 2006-2008 by Tim Niemueller, AllemaniACs RoboCup Team
+#                 2016 Sven Schneider, Tobias Neumann
 #
 #*****************************************************************************
 #
@@ -16,5 +17,15 @@
 ifneq ($(wildcard /usr/include/sys/inotify.h $(SYSROOT)/usr/include/*-linux-gnu*/sys/inotify.h),)
   HAVE_INOTIFY=1
   CFLAGS += -DHAVE_INOTIFY
+endif
+
+ifneq ($(wildcard $(BUILDCONFDIR)/utils/ipc/shm.h),)
+  CFLAGS += -DHAVE_UTILS_IPC_SHM
+  HAVE_UTILS_IPC_SHM=1
+endif
+
+ifneq ($(wildcard $(BUILDCONFDIR)/utils/time/clock.h),)
+  CFLAGS += -DHAVE_UTILS_TIME
+  HAVE_UTILS_TIME=1
 endif
 
